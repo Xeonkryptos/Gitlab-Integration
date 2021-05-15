16fa14a5a6fa7754fa13240228aa51b10d41aa68
Dennis Föller <XeonAppDev@gmail.com>
2021-05-15 17:27:43 +0200

    created action and assigned it to the corresponding menus (the action does nothing at the moment, though)

---
+++ src/main/kotlin/com/github/xeonkryptos/integration/gitlab/ui/projectLinker/ProjectLinkerAction.kt
@@ -0,0 +1,11 @@
+package com.github.xeonkryptos.integration.gitlab.ui.projectLinker
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class ProjectLinkerAction: AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        TODO("Open dialog to define the target gitlab server, account and path")
+    }
+}
